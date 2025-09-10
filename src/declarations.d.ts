f491dd735fcf994e21e3fecc3b94b3fac380ec7e
9Quiescent <Dkalongonda@gmail.com>
2025-09-11 00:18:59 +0930

    chore: fix bootstrap files (remove stray '>' prefixes)

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,1 +1,5 @@
-declare module "*.png"; declare module "*.jpg"; declare module "*.jpeg"; declare module "*.svg"; declare module "*.mp4";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.mp4";
